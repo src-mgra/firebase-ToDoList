5c0e7104c9dbc899433a56b293be405715e265b0
Micha G <25931746+src-mgra@users.noreply.github.com>
2018-03-15 16:15:38 +0100

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,48 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+
+import { HttpModule} from '@angular/http';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireModule } from 'angularfire2';
+import { FirebaseServiceProvider } from '../providers/firebase-service/firebase-service';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBRHQkt6NwAdNi_FvTadyi9j9XBXhp19Ms",
+  authDomain: "test-9d049.firebaseapp.com",
+  databaseURL: "https://test-9d049.firebaseio.com",
+  projectId: "test-9d049",
+  storageBucket: "test-9d049.appspot.com",
+  messagingSenderId: "746980784601"
+};
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    AngularFireDatabaseModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    FirebaseServiceProvider
+  ]
+})
+export class AppModule {}
